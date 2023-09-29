845cf340757f1b09306f3f7bd933b34c70a1e0be
mercy135 <42065455+mercy135@users.noreply.github.com>
2023-09-29 16:40:28 +0100

    Create Task3.sql

---
+++ Task3.sql
@@ -0,0 +1,9 @@
+/*
+Task 1: Data Download, Import, and Database Connection
+*/
+
+/*
+Task 2: What are the unique brands available in the dataset?
+
+We are enquiring about the unique brands available in the dataset to gain insights into the variety of products and manufacturers present in our sales records.
+This information can help us understand the market presence and popularity of different brands among our customers.
